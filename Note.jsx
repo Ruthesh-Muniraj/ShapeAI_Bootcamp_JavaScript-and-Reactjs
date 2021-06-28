49464bebc511868d926a3c23ff23f0a0f720db3a
Ruthesh Muniraj <rutheshmuniraj@outlook.com>
2021-06-28 09:24:34 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Note = () => {
+    return (
+        <div className="note">
+            <h1>React.js Bootcamp</h1>
+            <p>This is home work from shape AI, hope I get passed in the test.</p>
+        </div>
+    )
+}
+
+export default Note
